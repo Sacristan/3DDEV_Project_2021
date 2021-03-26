bf80688817b0452b537a74ebeb615772f79190f0
Sacristan <girts.kesteris@gmail.com>
2021-03-26 11:57:58 +0200

    init commit

---
+++ Assets/_Game/Src/NewBehaviourScript.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    void Awake()
+    {
+        Debug.Log("AWAKE");
+    }
+
+    void Start()
+    {
+        Debug.Log("START");
+
+    }
+
+    void Update()
+    {
+        Debug.Log("UPDATE");
+    }
+}
