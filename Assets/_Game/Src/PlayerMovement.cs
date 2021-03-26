e87398eba4b8c35a7d0c21574717db3ecf5ad4c3
Sacristan <girts.kesteris@gmail.com>
2021-03-26 14:55:49 +0200

    player movement

---
+++ Assets/_Game/Src/PlayerMovement.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] float speed = 10f;
+    [SerializeField] float rotationSensitivity = 10f;
+    CharacterController _characterController;
+    Camera _camera;
+
+    float jaw;
+    float pitch;
+
+    void Start()
+    {
+        _characterController = GetComponent<CharacterController>();
+        _camera = GetComponentInChildren<Camera>();
+
+        // pitch = transform.localEulerAngles.x;
+        // jaw = transform.localEulerAngles.y;
+    
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        float mouseHorizontal = Input.GetAxis("Mouse X");
+        float mouseVertical = Input.GetAxis("Mouse Y");
+
+        jaw += mouseHorizontal * rotationSensitivity * Time.deltaTime;
+        pitch -= mouseVertical * rotationSensitivity * Time.deltaTime;
+
+        pitch = Mathf.Clamp(pitch, -90f, 90f);
+
+        Vector3 inputOnAxis = new Vector3(horizontal, 0, vertical);
+        Vector3 motion = inputOnAxis * speed * Time.deltaTime;
+        motion += Physics.gravity * Time.deltaTime;
+
+        transform.localRotation = Quaternion.Euler(Vector3.up * jaw);
+        _characterController.Move(motion);
+    }
+
+    private void LateUpdate()
+    {
+        _camera.transform.localRotation = Quaternion.Euler(Vector3.right * pitch);
+    }
+
+}
